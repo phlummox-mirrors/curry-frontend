02ab124f2d646ff9edef2186593ab5ab25949e0c
Michael Hanus <mh@informatik.uni-kiel.de>
2006-02-06 09:33:25 +0000

    Code simplified and generalized to tuples of arbitrary arity.

--- src/PatchPrelude.hs
+++ src/PatchPrelude.hs
@@ -18,77 +18,20 @@ patchPreludeFCY (Prog name imports types funcs ops)
      = Prog name imports types funcs ops
 
 prelude_types_fcy :: [TypeDecl]
-prelude_types_fcy = [(Type ("prelude","()") Public [] 
-		      [(Cons ("prelude","()") 0 Public [])]),
-		     (Type ("prelude","[]") Public [0] 
-		      [(Cons ("prelude","[]") 0 Public []),
-		       (Cons ("prelude",":") 2 Public 
-			[(TVar 0),(TCons ("prelude","[]") [(TVar 0)])])]),
-		     (Type ("prelude","(,)") Public [0,1] 
-		      [(Cons ("prelude","(,)") 2 Public 
-			[(TVar 0),(TVar 1)])]),
-		     (Type ("prelude","(,,)") Public [0,1,2]
-		      [(Cons ("prelude","(,,)") 3 Public 
-			[(TVar 0),(TVar 1),(TVar 2)])]),
-		     (Type ("prelude","(,,,)") Public [0,1,2,3] 
-		      [(Cons ("prelude","(,,,)") 4 Public 
-			[(TVar 0),(TVar 1),(TVar 2),(TVar 3)])]),
-		     (Type ("prelude","(,,,,)") Public [0,1,2,3,4] 
-		      [(Cons ("prelude","(,,,,)") 5 Public 
-			[(TVar 0),(TVar 1),(TVar 2),(TVar 3),(TVar 4)])]),
-		     (Type ("prelude","(,,,,,)") Public [0,1,2,3,4,5] 
-		      [(Cons ("prelude","(,,,,,)") 6 Public 
-			[(TVar 0),(TVar 1),(TVar 2),(TVar 3),(TVar 4),
-			 (TVar 5)])]),
-		     (Type ("prelude","(,,,,,,)") Public [0,1,2,3,4,5,6] 
-		      [(Cons ("prelude","(,,,,,,)") 7 Public 
-			[(TVar 0),(TVar 1),(TVar 2),(TVar 3),
-			 (TVar 4),(TVar 5),(TVar 6)])]),
-		     (Type ("prelude","(,,,,,,,)") Public [0,1,2,3,4,5,6,7] 
-		      [(Cons ("prelude","(,,,,,,,)") 8 Public 
-			[(TVar 0),(TVar 1),(TVar 2),(TVar 3),
-			 (TVar 4),(TVar 5),(TVar 6),(TVar 7)])]),
-		     (Type ("prelude","(,,,,,,,,)") Public [0,1,2,3,4,5,6,7,8] 
-		      [(Cons ("prelude","(,,,,,,,,)") 9 Public
-			[(TVar 0),(TVar 1),(TVar 2),(TVar 3),
-			 (TVar 4),(TVar 5),(TVar 6),(TVar 7),(TVar 8)])]),
-		     (Type ("prelude","(,,,,,,,,,)") Public 
-		      [0,1,2,3,4,5,6,7,8,9] 
-		      [(Cons ("prelude","(,,,,,,,,,)") 10 Public 
-			[(TVar 0),(TVar 1),(TVar 2),(TVar 3),
-			 (TVar 4),(TVar 5),(TVar 6),(TVar 7),(TVar 8),(TVar 9)])]),
-		     (Type ("prelude","(,,,,,,,,,,)") Public 
-		      [0,1,2,3,4,5,6,7,8,9,10] 
-		      [(Cons ("prelude","(,,,,,,,,,,)") 11 Public 
-			[(TVar 0),(TVar 1),(TVar 2),(TVar 3),
-			 (TVar 4),(TVar 5),(TVar 6),(TVar 7),(TVar 8),
-			 (TVar 9),(TVar 10)])]),
-		     (Type ("prelude","(,,,,,,,,,,,)") Public 
-		      [0,1,2,3,4,5,6,7,8,9,10,11] 
-		      [(Cons ("prelude","(,,,,,,,,,,,)") 12 Public 
-			[(TVar 0),(TVar 1),(TVar 2),(TVar 3),
-			 (TVar 4),(TVar 5),(TVar 6),(TVar 7),(TVar 8),
-			 (TVar 9),(TVar 10),(TVar 11)])]),
-		     (Type ("prelude","(,,,,,,,,,,,,)") Public 
-		      [0,1,2,3,4,5,6,7,8,9,10,11,12] 
-		      [(Cons ("prelude","(,,,,,,,,,,,,)") 13 Public 
-			[(TVar 0),(TVar 1),(TVar 2),(TVar 3),
-			 (TVar 4),(TVar 5),(TVar 6),(TVar 7),
-			 (TVar 8),(TVar 9),(TVar 10),(TVar 11),(TVar 12)])]),
-		     (Type ("prelude","(,,,,,,,,,,,,,)") Public 
-		      [0,1,2,3,4,5,6,7,8,9,10,11,12,13] 
-		      [(Cons ("prelude","(,,,,,,,,,,,,,)") 14 Public 
-			[(TVar 0),(TVar 1),(TVar 2),(TVar 3),
-			 (TVar 4),(TVar 5),(TVar 6),(TVar 7),
-			 (TVar 8),(TVar 9),(TVar 10),(TVar 11),
-			 (TVar 12),(TVar 13)])]),
-		     (Type ("prelude","(,,,,,,,,,,,,,,)") Public 
-		      [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14] 
-		      [(Cons ("prelude","(,,,,,,,,,,,,,,)") 15 Public 
-			[(TVar 0),(TVar 1),(TVar 2),(TVar 3),
-			 (TVar 4),(TVar 5),(TVar 6),(TVar 7),
-			 (TVar 8),(TVar 9),(TVar 10),(TVar 11),
-			 (TVar 12),(TVar 13),(TVar 14)])])]
+prelude_types_fcy =
+  [Type ("prelude","()") Public [] [(Cons ("prelude","()") 0 Public [])],
+   Type ("prelude","[]") Public [0] 
+        [Cons ("prelude","[]") 0 Public [],
+         Cons ("prelude",":") 2 Public 
+              [TVar 0, TCons ("prelude","[]") [TVar 0]]]] ++
+  map tupleType [2..maxTupleArity]
+
+tupleType ar = let tuplecons = "("++take (ar-1) (repeat ',')++")" in
+  Type ("prelude",tuplecons) Public [0..ar-1]
+       [Cons ("prelude",tuplecons) ar Public (map TVar [0..ar-1])]
+
+-- Maximal arity of tuples:
+maxTupleArity = 15
 
 
 -------------------------------------------------------------------------------
