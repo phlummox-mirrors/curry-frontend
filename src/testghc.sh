b0d1b7254c3fb15e77a8ca5ddb5e3aeb37df5d78
Bernd Braßel <bbr@informatik.uni-kiel.de>
2007-11-14 10:47:47 +0000

    version greater than 6.6 supported

--- src/testghc.sh
+++ src/testghc.sh
@@ -3,9 +3,14 @@
 [ -f "Makefile_support" ] && rm -f Makefile_support
 
 GHCVS=`ghc -V`
-GHCV=`expr "$GHCVS" : ".*version \([0-9]\.[0-9]\).*"`
+GHCMajorV=`expr "$GHCVS" : ".*version \([0-9]*\).*"`
+GHCMinorV=`expr "$GHCVS" : ".*version [0-9]*\.\([0-9]*\).*"`
 
-if [ "$GHCV" != "6.6" ]; then
+
+if [[ $GHCMajorV -lt 6 ]]; then
+  echo "# Just for ghc < 6" > Makefile_support 
+  echo "HC_OPTS	+= -syslib lang" >> Makefile_support
+elif [[ $GHCMajorV -eq 6 && $GHCMinorV -lt 6 ]]; then 
   echo "# Just for ghc < 6.6" > Makefile_support 
   echo "HC_OPTS	+= -syslib lang" >> Makefile_support
 fi
