e675fd9462fc97afa35a7135394905600e708ee6
Michael Hanus <mh@informatik.uni-kiel.de>
2009-03-17 16:12:09 +0000

    Syntax changed to standard test syntax due to problems on mouton.

--- src/testghc.sh
+++ src/testghc.sh
@@ -7,10 +7,10 @@ GHCMajorV=`expr "$GHCVS" : ".*version \([0-9]*\).*"`
 GHCMinorV=`expr "$GHCVS" : ".*version [0-9]*\.\([0-9]*\).*"`
 
 
-if [[ $GHCMajorV -lt 6 ]]; then
+if test $GHCMajorV -lt 6 ; then
   echo "# Just for ghc < 6" > Makefile_support 
   echo "HC_OPTS	+= -syslib lang" >> Makefile_support
-elif [[ $GHCMajorV -eq 6 && $GHCMinorV -lt 6 ]]; then 
+elif  test $GHCMajorV -eq 6 -a $GHCMinorV -lt 6 ; then 
   echo "# Just for ghc < 6.6" > Makefile_support 
   echo "HC_OPTS	+= -syslib lang" >> Makefile_support
 fi
