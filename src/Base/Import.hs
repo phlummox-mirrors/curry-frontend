9f4fa140f632c6731ca7152bd916f688a18bbd15
Björn Peemöller <bjp@informatik.uni-kiel.de>
2011-05-20 11:31:36 +0200

    Import converted to normal Haskell file

--- src/Base/Import.hs
+++ src/Base/Import.hs
@@ -3,20 +3,24 @@
 
 > module Base.Import
 >   ( ImportEnv, bindAlias, lookupAlias, sureLookupAlias, initIEnv
+>   , fromDeclList
 >   ) where
 
 > import qualified Data.Map as Map (Map, empty, insert, lookup)
 > import Data.Maybe (fromMaybe)
 
-> import Curry.Base.Ident
-> import qualified Curry.Syntax as CS
+> import Curry.Base.Ident (ModuleIdent)
+> import qualified Curry.Syntax as CS (Decl (..))
 
 > type ImportEnv = Map.Map ModuleIdent ModuleIdent
 
 > bindAlias :: CS.Decl -> ImportEnv -> ImportEnv
-> bindAlias (CS.ImportDecl _ mid _ mmid _)
->    = Map.insert mid (fromMaybe mid mmid)
+> bindAlias (CS.ImportDecl _ mid _ alias _)
+>   = Map.insert mid $ fromMaybe mid alias
 > bindAlias _ = error "Base.bindAlias: no import declaration"
+
+> fromDeclList :: [CS.Decl] -> ImportEnv
+> fromDeclList = foldr bindAlias initIEnv
 
 > lookupAlias :: ModuleIdent -> ImportEnv -> Maybe ModuleIdent
 > lookupAlias = Map.lookup
