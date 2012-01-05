a443e8f26586e71f2a35198b7edc52f3509694f7
Björn Peemöller <bjp@informatik.uni-kiel.de>
2012-01-05 15:20:53 +0100

    Impreoved check script

--- test/check.sh
+++ test/check.sh
@@ -4,10 +4,34 @@ subdir=".curry"
 compilers="cymake cymake_pakcs"
 modules="*.curry"
 targets="flat xml acy uacy"
+importdir="."
 
-if [ $# -eq 1 ]; then
-  targets=$1
-fi
+function usage()
+{
+  echo "Usage: check.sh [OPTIONS] modules"
+  echo "Compare old and new frontend against each other"
+  echo ""
+  echo "  -i DIR  , --import-dir DIR   Search for libraries in DIR"
+  echo "  -i EXTS , --targets EXTS     Create the target types EXTS (some of flat, xml, acy, uacy)"
+  echo "  -h      , --help             Show this help and exit"
+}
+
+while [ "$1" != "" ]; do
+  case $1 in
+    -i | --import-dir  )    shift
+                            importdir=$1
+                            ;;
+    -t | --targets     )    shift
+                            targets=$1
+                            ;;
+    -h | --help )           usage
+                            exit
+                            ;;
+    * )                     modules=$*
+                            break
+  esac
+  shift
+done
 
 for comp in $compilers; do
   echo -e "$comp\n============"
@@ -23,7 +47,7 @@ for comp in $compilers; do
   # compile targets
   for mdl in $modules; do
     for tgt in $targets; do
-      $comp -e --$tgt $mdl
+      $comp -e -i $importdir --$tgt $mdl
     done
   done
 done
