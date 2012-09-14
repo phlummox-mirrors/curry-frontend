d688c1894ca6d30fd3deb9006f42f78df31afa7f
Björn Peemöller <bjp@informatik.uni-kiel.de>
2012-09-14 09:49:34 +0200

    Compiler option renamed

--- src/CompilerOpts.hs
+++ src/CompilerOpts.hs
@@ -105,7 +105,7 @@ data DumpLevel
   | DumpSyntaxChecked -- ^ dump source code after syntax checking
   | DumpPrecChecked   -- ^ dump source code after precedence checking
   | DumpTypeChecked   -- ^ dump source code after type checking
-  | DumpRenamed       -- ^ dump source  after renaming
+  | DumpQualified     -- ^ dump source  after qualification
   | DumpDesugared     -- ^ dump source  after desugaring
   | DumpSimplified    -- ^ dump source  after simplification
   | DumpLifted        -- ^ dump source  after lambda-lifting
@@ -119,7 +119,7 @@ dumpLevel = [ (DumpParsed       , "parsed", "parse tree"               )
             , (DumpSyntaxChecked, "sc"    , "syntax checker output"    )
             , (DumpPrecChecked  , "pc"    , "precedence checker output")
             , (DumpTypeChecked  , "tc"    , "type checker output"      )
-            , (DumpRenamed      , "rn"    , "renamer output"           )
+            , (DumpQualified    , "qual"  , "qualifier output"         )
             , (DumpDesugared    , "ds"    , "desugarer output"         )
             , (DumpSimplified   , "simpl" , "simplifier output"        )
             , (DumpLifted       , "lifted", "lifting output"           )
