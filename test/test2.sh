1e1df05cc7dc1f4b099df999231d51e81ee160d7
Matthias Böhm <mboe@informatik.uni-kiel.de>
2013-06-05 11:31:09 +0200

    included more test files in third test

--- test/test2.sh
+++ test/test2.sh
@@ -3,8 +3,11 @@ cymake=../dist/build/cymake/cymake
 cymake_orig=../dist/build/cymake/cymake_orig
 
 echo ================
-
-for file in Annot7 DictTrans2 DictTrans3 Annot6 DictTrans8 BugDicts BugDictTrans8 Annot_bug 
+  
+for file in DictTrans1 DictTrans2 DictTrans3 DictTrans4 DictTrans5 DictTrans6 DictTrans7 DictTrans8\
+  Annot1 Annot2 Annot3 Annot4 Annot5 Annot6 Annot7\
+  BugDicts BugDictTrans8 Annot_bug Annot_bug3
 do
+  #echo $file
   $cymake -f typeclasses/$file.curry 2> /dev/null 1> /dev/null || echo Error in $file.curry
 done
