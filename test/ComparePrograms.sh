25209b10eb9e28e37f5ba6ad3f2203529c02ed8d
Matthias Böhm <mboe@informatik.uni-kiel.de>
2013-06-17 10:07:45 +0200

    compare also interface files in test of equality of generated flat curry programs

--- test/ComparePrograms.sh
+++ test/ComparePrograms.sh
@@ -1,6 +1,7 @@
 cmpPrograms=../dist/build/cmpPrograms/cmpPrograms
 
 (cd lib/.curry && ls *.fcy) | xargs -i -n 1 $cmpPrograms $1/.curry/'{}' "$1"2/.curry/'{}' | tee errors.txt
+(cd lib/.curry && ls *.fint) | xargs -i -n 1 $cmpPrograms $1/.curry/'{}' "$1"2/.curry/'{}' | tee -a errors.txt
 
 echo "============================="
 echo "Errors: "
