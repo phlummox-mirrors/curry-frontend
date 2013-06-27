f3ed3370b5d754be58820bb29167c4c47fa13a22
Matthias Böhm <mboe@informatik.uni-kiel.de>
2013-06-27 12:06:48 +0200

    in test #1: first compile test files with original cymake, then with new cymake

--- test/test.sh
+++ test/test.sh
@@ -3,13 +3,20 @@ cymake=../../dist/build/cymake/cymake
 cymake_orig=../../dist/build/cymake/cymake_orig
 
 
-pushd lib && $cymake -e -f *.curry && popd
-
 if [ "$1" = all ]
 then
+  echo "=================="
   echo "building with orig"
+  echo "=================="
   pushd lib2 && $cymake_orig -e -f *.curry && popd
   #pushd lib2/.curry/ && sed 's/$/ Nothing/' -s -i *.fint *.fcy && popd
 fi
+
+echo "=================="
+echo "build with current"
+echo "=================="
+
+pushd lib && $cymake -e -f *.curry && popd
+
 
 ./ComparePrograms.sh lib
