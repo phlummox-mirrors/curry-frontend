bc991d7698bb8bd0efdb44beeacdbc70e2b11290
Matthias Böhm <mboe@informatik.uni-kiel.de>
2013-06-27 09:14:10 +0200

    in cmpPrograms: added better error message when fromJust fails

--- src/cmpPrograms.hs
+++ src/cmpPrograms.hs
@@ -3,18 +3,24 @@ module Main where
 
 import System.Exit
 import System.Environment
-import Data.Maybe
 import Curry.ExtendedFlat.Type
 
 import Curry.ExtendedFlat.Equality
-import Control.Monad
+
+fromJust' :: String -> IO (Maybe a) -> IO a
+fromJust' err m = do
+  val <- m 
+  case val of 
+    Nothing -> 
+      error ("\n\n\n=========\nerror: fromJust'!\n" ++ err ++ "\n=========\n\n\n")
+    Just x -> return x  
 
 main :: IO ()
 main = do
   args <- getArgs
   putStr ("Comparing \"" ++ (args !! 0) ++ "\" and \"" ++ (args !! 1) ++ "\": ")
-  p1 <- liftM fromJust $ readFlat (args !! 0)
-  p2 <- liftM fromJust $ readFlat (args !! 1)
+  p1 <- fromJust' (args !! 0) $ readFlat (args !! 0)
+  p2 <- fromJust' (args !! 1) $ readFlat (args !! 1)
   
   case progsEqual p1 p2 of
     (True, _) -> do putStrLn "Equal"; exitSuccess
