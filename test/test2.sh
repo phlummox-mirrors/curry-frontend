9a1f6c0bdf25c74a88c55b56405ec8a6511b0d9a
Matthias Böhm <mboe@informatik.uni-kiel.de>
2013-07-06 18:18:10 +0200

    test2.sh: added typeclasses directory to import directory in the call of cymake

--- test/test2.sh
+++ test/test2.sh
@@ -28,7 +28,7 @@ for file in DictTrans1 DictTrans2 DictTrans3 DictTrans4 \
   ImplicBug ExtendedExample
 do
   #echo $file
-  $cymake -f typeclasses/$file.curry 2>> output_test2_stderr.txt 1>> output_test2_stdout.txt || echo Error in $file.curry
+  $cymake -f -i typeclasses typeclasses/$file.curry 2>> output_test2_stderr.txt 1>> output_test2_stdout.txt || echo Error in $file.curry
 done
 
 
