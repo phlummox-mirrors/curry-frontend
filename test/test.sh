68c1f4fb9c2db4f8e8fdd0a5bc146bad71faca50
Matthias Böhm <mboe@informatik.uni-kiel.de>
2013-07-11 11:46:16 +0200

    adjusted test shell files: some small changes

--- test/test.sh
+++ test/test.sh
@@ -5,12 +5,12 @@ cymake_orig=../../dist/build/cymake/cymake_orig
 
 if [ "$1" = all ]
 then
-  rm -r lib2/.curry
+  rm -r -f lib2/.curry
  
   echo "=================="
   echo "building with orig"
   echo "=================="
-  pushd lib2 && $cymake_orig -e -f *.curry && popd
+  pushd lib2 ; $cymake_orig -e -f *.curry || exit 1; popd
   #pushd lib2/.curry/ && sed 's/$/ Nothing/' -s -i *.fint *.fcy && popd
 fi
 
@@ -18,9 +18,9 @@ echo "=================="
 echo "build with current"
 echo "=================="
 
-rm -r lib/.curry
+rm -r -f lib/.curry
 
-pushd lib && $cymake -e -f *.curry && popd
+pushd lib ; $cymake -e -f *.curry || exit 1; popd
 
 
 ./ComparePrograms.sh lib
