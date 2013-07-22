2c119335cea7f285eaa5f7831501f79a9f96c94f
Matthias Böhm <mboe@informatik.uni-kiel.de>
2013-07-22 18:39:06 +0200

    added new test files related to hidden flag

--- test/test2.sh
+++ test/test2.sh
@@ -62,9 +62,10 @@ for file in TestClassExports TestClassExports2 TestClassExportsImports \
   InstancesExports InstancesExports2 InstancesExportsImports InstancesExportBug InstancesExportBugImports \
   Dependencies1 Dependencies1Imports \
   ClassExportImport ClassExportImport1 ClassExportImport2 ClassExportImport3 ClassExportImport4 \
-                    ClassExportImport5 ClassExportImport6 ClassExportImport7 \
+                    ClassExportImport5 ClassExportImport6 ClassExportImport7 ClassExportImport8 \
   Dependencies2 Dependencies2Import1 Dependencies2Import2 Dependencies2Import3 \
-  Dependencies3 Dependencies3Import1 Dependencies3Import2 Dependencies3Import3
+  Dependencies3 Dependencies3Import1 Dependencies3Import2 Dependencies3Import3 \
+  HiddenNotHidden HiddenNotHiddenImport
 do
   echo $file >> tmp.txt
   $cymake -f -i typeclasses/modules typeclasses/modules/$file.curry 2>> output_test2_stderr.txt 1>> output_test2_stdout.txt || echo Error in $file.curry
---
+++ test/typeclasses/modules/HiddenNotHidden.curry
@@ -0,0 +1,8 @@
+
+
+module HiddenNotHidden (D(..)) where
+
+class C a where
+
+class C a => D a where
+
