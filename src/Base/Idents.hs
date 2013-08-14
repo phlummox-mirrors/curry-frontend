31b8f1614b1b1c73fe964da824af919fd0f540c7
Matthias Böhm <mboe@informatik.uni-kiel.de>
2013-08-14 11:35:28 +0200

    added new file with identifiers

---
+++ src/Base/Idents.hs
@@ -0,0 +1,93 @@
+{- |
+    Module      :  $Header$
+    Description :  Identifiers
+    Copyright   :  (c) 2013 Matthias Böhm
+    License     :  OtherLicense
+
+    Maintainer  :  bjp@informatik.uni-kiel.de
+    Stability   :  experimental
+    Portability :  portable
+    
+    This file contains some identifiers used in different places. 
+-}
+
+module Base.Idents where
+
+import Curry.Base.Ident
+import Base.Names
+import Curry.Syntax
+
+eqClsIdentName :: String
+eqClsIdentName = "Eq"
+
+ordClsIdentName :: String
+ordClsIdentName = "Ord"
+
+showClsIdentName :: String
+showClsIdentName = "Show"
+
+readClsIdentName :: String
+readClsIdentName = "Read"
+
+boundedClsIdentName :: String
+boundedClsIdentName = "Bounded"
+
+enumClsIdentName :: String
+enumClsIdentName = "Enum"
+
+mkTCPQIdent :: String -> QualIdent
+mkTCPQIdent = qualifyWith tcPreludeMIdent . mkIdent
+
+eqClsIdent :: QualIdent
+eqClsIdent = mkTCPQIdent eqClsIdentName 
+
+ordClsIdent :: QualIdent
+ordClsIdent = mkTCPQIdent ordClsIdentName
+
+showClsIdent :: QualIdent
+showClsIdent = mkTCPQIdent showClsIdentName
+
+readClsIdent :: QualIdent
+readClsIdent = mkTCPQIdent readClsIdentName
+
+boundedClsIdent :: QualIdent
+boundedClsIdent = mkTCPQIdent boundedClsIdentName
+
+enumClsIdent :: QualIdent
+enumClsIdent = mkTCPQIdent enumClsIdentName
+
+eqOp :: Ident
+eqOp = mkIdent "=="
+
+leqOp :: Ident
+leqOp = mkIdent "<="
+
+lessOp :: Ident
+lessOp = mkIdent "<"
+
+andOp :: Ident
+andOp = mkIdent "&&"
+
+orOp :: Ident
+orOp = mkIdent "||"
+
+infixEqOp :: InfixOp
+infixEqOp = InfixOp Nothing $ qualifyWith dummyMIdent $ eqOp
+
+infixLeqOp :: InfixOp
+infixLeqOp = InfixOp Nothing $ qualifyWith dummyMIdent $ leqOp
+
+infixLessOp :: InfixOp
+infixLessOp = InfixOp Nothing $ qualifyWith dummyMIdent $ lessOp
+
+infixAndOp :: InfixOp
+infixAndOp = InfixOp Nothing $ qualifyWith dummyMIdent $ andOp
+
+infixOrOp :: InfixOp
+infixOrOp = InfixOp Nothing $ qualifyWith dummyMIdent $ orOp
+
+trueCons :: QualIdent
+trueCons = qualifyWith dummyMIdent $ mkIdent "True"
+
+falseCons :: QualIdent
+falseCons = qualifyWith dummyMIdent $ mkIdent "False"
