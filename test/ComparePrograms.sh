bfba4b81032d6e915f179ebd7d44fc8503b07bd5
Matthias Böhm <mboe@informatik.uni-kiel.de>
2013-08-21 08:34:19 +0200

    added usage info and success message to ComparePrograms.sh

--- test/ComparePrograms.sh
+++ test/ComparePrograms.sh
@@ -1,12 +1,32 @@
+
+# usage: cmpPrograms <lib dir>
+
+if [ ! $1 ]
+then
+  echo Usage: ComparePrograms \<test files directory\>
+  exit 1
+fi
+
 cmpPrograms=../dist/build/cmpPrograms/cmpPrograms
 
-(cd lib/.curry && ls *.fcy) | xargs -i -n 1 $cmpPrograms $1/.curry/'{}' "$1"2/.curry/'{}' | tee errors.txt
-(cd lib/.curry && ls *.fint) | xargs -i -n 1 $cmpPrograms $1/.curry/'{}' "$1"2/.curry/'{}' | tee -a errors.txt
+(cd $1/.curry && ls *.fcy) | xargs -i -n 1 $cmpPrograms $1/.curry/'{}' "$1"2/.curry/'{}' | tee errors.txt
+(cd $1/.curry && ls *.fint) | xargs -i -n 1 $cmpPrograms $1/.curry/'{}' "$1"2/.curry/'{}' | tee -a errors.txt
 
-echo "============================="
-echo "Errors: "
-echo "============================="
+(cat errors.txt | grep "Not equal") > errors2.txt
 
-cat errors.txt | grep "Not equal"
+if [ -s errors2.txt ]
+then
+
+  echo "============================="
+  echo "Errors: "
+  echo "============================="
+
+  cat errors2.txt
+else
+  echo "============================="
+  echo "Test successfully passed"
+fi
+
+rm errors2.txt
 
 exit 0
