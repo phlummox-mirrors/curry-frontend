a7be72fbe5593a7efeb03edca252ec5e6c194640
Matthias Böhm <mboe@informatik.uni-kiel.de>
2013-08-30 17:04:05 +0200

    moved lib and lib2 dir into typeclasses dir, and renamed the dirs to "comparetest_orig" and "comparetest_new"
    also adjusted test files accordingly

--- test/ComparePrograms.sh
+++ test/ComparePrograms.sh
@@ -1,16 +1,17 @@
 
 # usage: cmpPrograms <lib dir>
 
-if [ ! $1 ]
+if [ ! $1 -o ! $2 ]
 then
-  echo Usage: ComparePrograms \<test files directory\>
+  echo Usage: ComparePrograms \<test files directory 1 \(orig\)\>
+  echo "                      " \<test files directory 2 \(new\)\>
   exit 1
 fi
 
 cmpPrograms=../dist/build/cmpPrograms/cmpPrograms
 
-(cd $1/.curry && ls *.fcy) | xargs -i -n 1 $cmpPrograms $1/.curry/'{}' "$1"2/.curry/'{}' | tee errors.txt
-(cd $1/.curry && ls *.fint) | xargs -i -n 1 $cmpPrograms $1/.curry/'{}' "$1"2/.curry/'{}' | tee -a errors.txt
+(cd $1/.curry && ls *.fcy) | xargs -i -n 1 $cmpPrograms $1/.curry/'{}' $2/.curry/'{}' | tee errors.txt
+(cd $1/.curry && ls *.fint) | xargs -i -n 1 $cmpPrograms $1/.curry/'{}' $2/.curry/'{}' | tee -a errors.txt
 
 (cat errors.txt | grep "Not equal") > errors2.txt
 
--- test/test1.sh
+++ test/test1.sh
@@ -13,33 +13,34 @@ then
   echo added, or this script is run the first time, this script must be called
   echo with the all option.
   echo
-  echo The test files are in the directories lib and lib2\; in lib2, the test
-  echo files are compiled with the original compiler, in lib, the test files
-  echo are compiled with the new compiler
-  exit
+  echo The test files are in the directories typeclasses/comparetest_orig and
+  echo typeclasses/comparetest_new\; in comparetest_orig, the test
+  echo files are compiled with the original compiler, in comparetest_new,
+  echo the test files are compiled with the new compiler. 
+  exit 1
 fi
 
 cymake_orig=`readlink -f $1`
-cymake=../../dist/build/cymake/cymake
+cymake=`readlink -f ../dist/build/cymake/cymake`
 
 if [ "$2" = all ]
 then
-  rm -r -f lib2/.curry
+  rm -r -f typeclasses/comparetest_orig/.curry
  
   echo "=================="
   echo "building with orig"
   echo "=================="
-  pushd lib2 ; $cymake_orig -e -f *.curry || exit 1; popd
-  #pushd lib2/.curry/ && sed 's/$/ Nothing/' -s -i *.fint *.fcy && popd
+  pushd typeclasses/comparetest_orig ; $cymake_orig -e -f *.curry || exit 1; popd
+  #pushd typeclasses/comparetest_orig/.curry/ && sed 's/$/ Nothing/' -s -i *.fint *.fcy && popd
 fi
 
 echo "=================="
 echo "build with current"
 echo "=================="
 
-rm -r -f lib/.curry
+rm -r -f typeclasses/comparetest_new/.curry
 
-pushd lib ; $cymake -e -f *.curry || exit 1; popd
+pushd typeclasses/comparetest_new ; $cymake -e -f *.curry || exit 1; popd
 
 
-./ComparePrograms.sh lib
+./ComparePrograms.sh typeclasses/comparetest_orig typeclasses/comparetest_new
