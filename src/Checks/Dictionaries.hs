d3a14f817390d5adab50d0f5849cc9ab09ccbc8e
Matthias Böhm <mboe@informatik.uni-kiel.de>
2013-11-04 08:37:51 +0100

    improved code layout in Dictionaries.hs

--- src/Checks/Dictionaries.hs
+++ src/Checks/Dictionaries.hs
@@ -43,11 +43,11 @@ import Control.Monad.State as S
 type DI = S.State DIState
 
 data DIState = DIState 
-  { mdl         :: ModuleIdent
-  , theClassEnv :: ClassEnv
-  , theValueEnv :: ValueEnv
+  { mdl           :: ModuleIdent
+  , theClassEnv   :: ClassEnv
+  , theValueEnv   :: ValueEnv
   , typeClassExts :: Bool
-  , errors      :: [Message]
+  , errors        :: [Message]
   }
 
 initState :: ModuleIdent -> ClassEnv -> ValueEnv -> Bool -> DIState
@@ -96,10 +96,10 @@ diDecl :: BT.Context -> Decl -> DI Decl
 diDecl cx (FunctionDecl p (Just cty@(cx', _)) n id0 eqs) = do
   cEnv <- getClassEnv
   vEnv <- getValueEnv
-  m <- getModuleIdent
+  m    <- getModuleIdent
   let -- we have to reduce the context before adding dictionary parameters, 
       -- because the recorded context is the "raw" context 
-      cx'' = reduceContext cEnv $ mirrorBF cx'
+      cx''  = reduceContext cEnv $ mirrorBF cx'
       cx''' = removeNonLocal vEnv (qualify id0) (qualLookupValue' m) cx''
   FunctionDecl p (Just cty) n id0 `liftM` (mapM (diEqu (cx ++ cx''') cx''') eqs)
 diDecl _ (FunctionDecl _ Nothing _ _ _) = internalError "no type info in diDecl"
@@ -126,12 +126,12 @@ removeNonLocal vEnv id0 lookup0 cx = newCx
 -- Takes the type of the corresponding function declaration, and the name
 -- of the function 
 diEqu :: BT.Context -> BT.Context -> Equation -> DI Equation
-diEqu cxAll cxLocal (Equation p lhs rhs)= 
+diEqu cxAll cxLocal (Equation p lhs rhs) = 
   liftM2 (Equation p) (diLhs cxLocal lhs) (diRhs cxAll rhs)
 
 -- |transform right hand sides
 diRhs :: BT.Context -> Rhs -> DI Rhs
-diRhs cx (SimpleRhs p e ds) = 
+diRhs cx (SimpleRhs  p e ds) = 
   liftM2 (SimpleRhs p) (diExpr cx e) (mapM (diDecl cx) ds)
 diRhs cx (GuardedRhs ces ds) = 
   liftM2 GuardedRhs (mapM (diCondExpr cx) ces) (mapM (diDecl cx) ds)
@@ -144,12 +144,12 @@ diCondExpr cx (CondExpr p e1 e2) =
   
 -- | transform left hand sides
 diLhs :: BT.Context -> Lhs -> DI Lhs
-diLhs cx (FunLhs id0 ps) = 
+diLhs cx (FunLhs   id0 ps) = 
   return $ FunLhs id0 (dictParams ++ ps)  
   where 
   dictParams = map (VariablePattern . contextElemToVar) cx
 diLhs cx (OpLhs p1 id0 p2) = diLhs cx (FunLhs id0 [p1, p2])
-diLhs cx a@(ApLhs _ _) =
+diLhs cx a@(ApLhs     _ _) =
   let (f, ps) = flatLhs a
   in diLhs cx (FunLhs f ps)  
 
@@ -159,32 +159,32 @@ diExpr cx e@(Literal l) = do
   exts <- typeClassExtensions
   case exts of
     False -> return e
-    True -> diLiteral cx l
+    True  -> diLiteral cx l
 diExpr cx0 v@(Variable (Just varCty0) qid) = do 
   checkForAmbiguousInstances (qidPosition qid) (mirrorBF $ fst varCty0)
   cEnv <- getClassEnv
   vEnv <- getValueEnv
-  m <- getModuleIdent
+  m    <- getModuleIdent
   
   let 
-    abstrCode = do
+    abstrCode     = do
       let varCty = (removeNonLocal vEnv qid (qualLookupValue' m) $ mirrorBF $ fst varCty0, snd varCty0)
           -- check whether we have a class method
-          cx = if isNothing $ maybeCls then fst varCty else mirrorBF $ fst varCty0
-          codes = map (concreteCode . dictCode cEnv cx0) cx 
+          cx     = if isNothing $ maybeCls then fst varCty else mirrorBF $ fst varCty0
+          codes  = map (concreteCode . dictCode cEnv cx0) cx 
       return codes
-    maybeCls = case qualLookupValue qid vEnv of
+    maybeCls      = case qualLookupValue qid vEnv of
       [Value _ _ _ cls'] -> cls'
       _ -> case qualLookupValue (qualQualify m qid) vEnv of
         [Value _ _ _ cls'] -> cls'
         _ -> internalError ("no function/method in Dictionaries: " ++ show qid) 
-    cls = fromJust $ maybeCls
+    cls           = fromJust $ maybeCls
     -- if we have a class function, transform this into the appropriate selector
     -- function
     isClassMethod = isJust $ maybeCls
-    zeroArity  = (arrowArity $ typeSchemeToType $ 
+    zeroArity     = (arrowArity $ typeSchemeToType $ 
       fromJust $ canonLookupMethodTypeScheme' cEnv cls (unqualify qid)) == 0
-    var'' = if not isClassMethod 
+    var''         = if not isClassMethod 
       then v
       -- Unqualify "qid"! The name of the selection function is still unique
       -- because the class name is unique 
@@ -193,7 +193,7 @@ diExpr cx0 v@(Variable (Just varCty0) qid) = do
              
   
   case (isClassMethod && isNothing (canonLookupMethodTypeScheme' cEnv cls (unqualify qid))) of
-    True -> do
+    True  -> do
       report $ errClassNeeded (qidPosition qid) cls
       return v
     False -> do 
@@ -205,78 +205,78 @@ diExpr cx0 v@(Variable (Just varCty0) qid) = do
         else code
       
   
-diExpr _ (Variable Nothing _) = internalError "diExpr: no type info"
-diExpr _ e@(Constructor _) = return e
-diExpr cx (Paren e) = Paren `liftM` diExpr cx e
-diExpr cx (Typed cty e cxt t) = liftM3 (Typed cty) (diExpr cx e) (return cxt) (return t)
-diExpr cx (Tuple sref es) = Tuple sref `liftM` (mapM (diExpr cx) es)   
-diExpr cx (List srefs es) = List srefs `liftM` (mapM (diExpr cx) es)
+diExpr _  (Variable  Nothing _) = internalError "diExpr: no type info"
+diExpr _  e@(Constructor     _) = return e
+diExpr cx (Paren             e) = Paren `liftM` diExpr cx e
+diExpr cx (Typed   cty e cxt t) = liftM3 (Typed cty) (diExpr cx e) (return cxt) (return t)
+diExpr cx (Tuple       sref es) = Tuple sref `liftM` (mapM (diExpr cx) es)   
+diExpr cx (List       srefs es) = List srefs `liftM` (mapM (diExpr cx) es)
 diExpr cx (ListCompr sref e ss) = 
   liftM2 (ListCompr sref) (diExpr cx e) (mapM (diStmt cx) ss) 
-diExpr cx (EnumFrom cty e1) = do
+diExpr cx (EnumFrom             cty e1) = do
   exts <- typeClassExtensions
   case exts of
     False -> EnumFrom cty `liftM` (diExpr cx e1) 
-    True -> diExpr cx (Apply (Variable cty tcPreludeEnumFromQIdent) e1)
-diExpr cx (EnumFromThen cty e1 e2) = do
+    True  -> diExpr cx (Apply (Variable cty tcPreludeEnumFromQIdent) e1)
+diExpr cx (EnumFromThen      cty e1 e2) = do
   exts <- typeClassExtensions
   case exts of
     False -> liftM2 (EnumFromThen cty) (diExpr cx e1) (diExpr cx e2)
-    True -> diExpr cx (Apply (Apply (Variable cty tcPreludeEnumFromThenQIdent) e1) e2)
-diExpr cx (EnumFromTo cty e1 e2) = do
+    True  -> diExpr cx (Apply (Apply (Variable cty tcPreludeEnumFromThenQIdent) e1) e2)
+diExpr cx (EnumFromTo        cty e1 e2) = do
   exts <- typeClassExtensions
   case exts of
     False -> liftM2 (EnumFromTo cty) (diExpr cx e1) (diExpr cx e2)
-    True -> diExpr cx (Apply (Apply (Variable cty tcPreludeEnumFromToQIdent) e1) e2)
+    True  -> diExpr cx (Apply (Apply (Variable cty tcPreludeEnumFromToQIdent) e1) e2)
 diExpr cx (EnumFromThenTo cty e1 e2 e3) = do
   exts <- typeClassExtensions
   case exts of
     False -> liftM3 (EnumFromThenTo cty) (diExpr cx e1) (diExpr cx e2) (diExpr cx e3)
-    True -> diExpr cx (Apply (Apply (Apply (Variable cty tcPreludeEnumFromThenToQIdent) e1) e2) e3)
-diExpr cx (UnaryMinus cty i e) = do
+    True  -> diExpr cx (Apply (Apply (Apply (Variable cty tcPreludeEnumFromThenToQIdent) e1) e2) e3)
+diExpr cx (UnaryMinus          cty i e) = do
   exts <- typeClassExtensions
   case exts of
     False -> UnaryMinus cty i `liftM` diExpr cx e
-    True -> diExpr cx (Apply (Variable cty negateQIdent) e)
-diExpr cx (Apply e1 e2) = liftM2 Apply (diExpr cx e1) (diExpr cx e2)
+    True  -> diExpr cx (Apply (Variable cty negateQIdent) e)
+diExpr cx (Apply           e1 e2) = liftM2 Apply (diExpr cx e1) (diExpr cx e2)
 -- adding dictionary parameters for the operator in InfixApply, Left- and RightSection
 -- expressions by transforming them into a term with Variable's and Apply's where
 -- the operator (or the flipped operator) is at the head position. 
 -- Note that infixOp preserves the type annotation of the operator!
-diExpr cx (InfixApply e1 op e2) = 
+diExpr cx (InfixApply   e1 op e2) = 
   diExpr cx (Apply (Apply (infixOp op) e1) e2)
-diExpr cx (LeftSection e op) = 
+diExpr cx (LeftSection      e op) = 
   diExpr cx (Apply (infixOp op) e)
-diExpr cx (RightSection op e) = 
+diExpr cx (RightSection     op e) = 
   diExpr cx (Apply (Apply prelFlip (infixOp op)) e)
-diExpr cx (Lambda sref ps e) = Lambda sref ps `liftM` diExpr cx e
+diExpr cx (Lambda      sref ps e) = Lambda sref ps `liftM` diExpr cx e
 -- pass context from outer scope
-diExpr cx (Let ds e) = liftM2 Let (mapM (diDecl cx) ds) (diExpr cx e)
-diExpr cx (Do ss e) = liftM2 Do (mapM (diStmt cx) ss) (diExpr cx e)
+diExpr cx (Let              ds e) = liftM2 Let (mapM (diDecl cx) ds) (diExpr cx e)
+diExpr cx (Do               ss e) = liftM2 Do (mapM (diStmt cx) ss) (diExpr cx e)
 diExpr cx (IfThenElse s e1 e2 e3) = 
   liftM3 (IfThenElse s) (diExpr cx e1) (diExpr cx e2) (diExpr cx e3)
-diExpr cx (Case s ct e alts) = 
+diExpr cx (Case      s ct e alts) = 
   liftM2 (Case s ct) (diExpr cx e) (mapM (diAlt cx) alts)
-diExpr cx (RecordConstr fs) = 
+diExpr cx (RecordConstr       fs) = 
   RecordConstr `liftM` (mapM (diField cx) fs)
 diExpr cx (RecordSelection e id0) = 
   flip RecordSelection id0 `liftM` diExpr cx e
-diExpr cx (RecordUpdate fs e) = 
+diExpr cx (RecordUpdate     fs e) = 
   liftM2 RecordUpdate (mapM (diField cx) fs) (diExpr cx e)
   
 -- |transform literals
 diLiteral :: BT.Context -> Literal -> DI Expression
 diLiteral _cx l@(Char   _ _) = return (Literal l)
 diLiteral _cx l@(String _ _) = return (Literal l)
-diLiteral cx l@(Int   v _) = do
+diLiteral cx  l@(Int    v _) = do
   vEnv <- getValueEnv
   let Value _ _ (ForAll _cxInt _n ty) _ : _ = lookupValue v vEnv
-      intTy = TypeConstructor (qualifyWith preludeMIdent $ mkIdent "Int") []
-      cxInt = [(numClsIdent, ty)] 
+      intTy   = TypeConstructor (qualifyWith preludeMIdent $ mkIdent "Int") []
+      cxInt   = [(numClsIdent, ty)] 
       newType = (cxInt, TypeArrow intTy ty)
   fromInt <- diExpr cx $ Variable (Just $ mirrorFB newType) fromIntegerQIdent
   return $ Apply fromInt (Literal l)
-diLiteral cx l@(Float v _) = do
+diLiteral cx  l@(Float  v _) = do
   vEnv <- getValueEnv
   let Value _ _ (ForAll _cxFloat _n ty) _ : _ = lookupValue v vEnv
       floatTy = TypeConstructor (qualifyWith preludeMIdent $ mkIdent "Float") []
@@ -288,8 +288,8 @@ diLiteral cx l@(Float v _) = do
 
 -- |transform statements
 diStmt :: BT.Context -> Statement -> DI Statement
-diStmt cx (StmtExpr s e) = StmtExpr s `liftM` diExpr cx e
-diStmt cx (StmtDecl ds) = StmtDecl `liftM` (mapM (diDecl cx) ds)
+diStmt cx (StmtExpr   s e) = StmtExpr s `liftM` diExpr cx e
+diStmt cx (StmtDecl    ds) = StmtDecl `liftM` (mapM (diDecl cx) ds)
 diStmt cx (StmtBind s p e) = StmtBind s p `liftM` diExpr cx e
 
 -- |transform alts
@@ -309,9 +309,9 @@ contextElemToVar (cls, ty) =
 
 -- creates concrete code from the abstract operations
 concreteCode :: Operation -> Expression
-concreteCode (Dictionary d) = 
+concreteCode (Dictionary           d) = 
   var' $ contextElemToVar d
-concreteCode (SelSuperClass d1 d2) =
+concreteCode (SelSuperClass    d1 d2) =
   Apply (var sel) (var' $ contextElemToVar d1)
   where sel = mkSelFunName (show $ fst d1) (show $ fst d2)  
 concreteCode (BuildDict (cls,ty) ops) = 
@@ -327,19 +327,19 @@ checkForAmbiguousInstances :: Position -> BT.Context -> DI ()
 checkForAmbiguousInstances p = mapM_ check
   where
   check :: (QualIdent, Type) -> DI ()
-  check (_cls, TypeVariable _) = ok
-  check (cls,  TypeConstructor ty _) = do
+  check (_cls, TypeVariable          _) = ok
+  check (cls , TypeConstructor    ty _) = do
     cEnv <- getClassEnv
     let insts = getInstances cEnv cls ty
     case insts of
       [] -> report $ errNoInstance p cls ty
       [_] -> ok
       (_:_:_) -> report $ errAmbiguousInstance p cls ty
-  check (cls,  TypeConstrained (t:_) _) = check (cls, t)
-  check (_cls, TypeConstrained [] _) = internalError "typeconstrained empty"
-  check (cls,  TypeArrow _ty1 _ty2) = check (cls, TypeConstructor qArrowIdP [])
-  check (_cls, TypeSkolem _) = internalError "typeSklolem"
-  check (_cls, TypeRecord _ _) = internalError "typerecord"
+  check (cls , TypeConstrained (t:_) _) = check (cls, t)
+  check (_cls, TypeConstrained []    _) = internalError "typeconstrained empty"
+  check (cls , TypeArrow     _ty1 _ty2) = check (cls, TypeConstructor qArrowIdP [])
+  check (_cls, TypeSkolem            _) = internalError "typeSklolem"
+  check (_cls, TypeRecord          _ _) = internalError "typerecord"
       
 
 
