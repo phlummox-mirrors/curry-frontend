2b51ba3a218f7e924b9203c6c95087d8f4ee8adf
Björn Peemöller <bjp@informatik.uni-kiel.de>
2014-10-20 16:10:14 +0200

    [HTML] Fixed bug in referencing entity definitions.
    In HTML5, the link to an entity, which is given as the href attribute
    of an anchor tag, may only contain unreserved characters (see RFC 3986).
    All other characters have to be "percent"-encoded. However, this is not
    true for the id attribute definining the target of the link, which may
    contain arbitrary (non-whitespace) characters. In consequence, the name
    of an entity (function/constructor) must be encoded for the link, but
    must not be encoded for the definition.

--- src/Html/CurryHtml.hs
+++ src/Html/CurryHtml.hs
@@ -106,16 +106,13 @@ code2html m defs c
   | isCall c  = (defs, maybe tag (addHtmlLink m tag) (getQualIdent c))
   | isDecl c  = case getQualIdent c of
       Just i | i `notElem` defs
-        -> (i:defs, spanTag (code2class c) (escIdent i) (escCode c))
+        -> (i:defs, spanTag (code2class c) (show $ unqualify i) (escCode c))
       _ -> (defs, tag)
   | otherwise = (defs, tag)
   where tag = spanTag (code2class c) "" (escCode c)
 
 escCode :: Code -> String
 escCode = htmlQuote . code2string
-
-escIdent :: QualIdent -> String
-escIdent = string2urlencoded . show . unqualify
 
 spanTag :: String -> String -> String -> String
 spanTag clV idV str
