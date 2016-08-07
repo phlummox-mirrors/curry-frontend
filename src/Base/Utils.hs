35b512436e92ee8895c856d91231e8b9d1f3fd1b
Finn Teegen <fte@informatik.uni-kiel.de>
2016-08-07 23:48:13 +0200

    Add new utility functions and remove others due to packages

--- src/Base/Utils.hs
+++ src/Base/Utils.hs
@@ -3,6 +3,7 @@
     Description :  Auxiliary functions
     Copyright   :  (c) 2001 - 2003 Wolfgang Lux
                        2011 - 2015 Björn Peemöler
+                       2016        Finn Teegen
     License     :  OtherLicense
 
     Maintainer  :  bjp@informatik.uni-kiel.de
@@ -15,7 +16,8 @@
 -}
 
 module Base.Utils
-  ( thd3, (++!), foldr2, mapAccumM, findDouble, concatMapM, findMultiples
+  ( fst3, snd3, thd3, curry3, uncurry3
+  , (++!), foldr2, findDouble, findMultiples
   ) where
 
 import Data.List (partition)
@@ -25,8 +27,20 @@ infixr 5 ++!
 -- The Prelude does not contain standard functions for triples.
 -- We provide projection, (un-)currying, and mapping for triples here.
 
+fst3 :: (a, b, c) -> a
+fst3 (x, _, _) = x
+
+snd3 :: (a, b, c) -> b
+snd3 (_, y, _) = y
+
 thd3 :: (a, b, c) -> c
 thd3 (_, _, z) = z
+
+curry3 :: ((a, b, c) -> d) -> a -> b -> c -> d
+curry3 f x y z = f (x,y,z)
+
+uncurry3 :: (a -> b -> c -> d) -> (a, b, c) -> d
+uncurry3 f (x, y, z) = f x y z
 
 -- The function (++!) is variant of the list concatenation operator (++)
 -- that ignores the second argument if the first is a non-empty list.
@@ -40,6 +54,7 @@ xs ++! ys = if null xs then ys else xs
 -- Fold operations with two arguments lists can be defined using
 -- zip and foldl or foldr, resp. Our definitions are unfolded for
 -- efficiency reasons.
+
 -- foldl2 :: (a -> b -> c -> a) -> a -> [b] -> [c] -> a
 -- foldl2 _ z []       _        = z
 -- foldl2 _ z _        []       = z
@@ -50,23 +65,10 @@ foldr2 _ z []       _        = z
 foldr2 _ z _        []       = z
 foldr2 f z (x : xs) (y : ys) = f x y (foldr2 f z xs ys)
 
--- The function 'mapAccumM' is a generalization of mapAccumL to monads
--- like foldM is for foldl.
-mapAccumM :: Monad m => (a -> b -> m (a, c)) -> a -> [b] -> m (a, [c])
-mapAccumM _ s []       = return (s, [])
-mapAccumM f s (x : xs) = do
-  (s' , y ) <- f s x
-  (s'', ys) <- mapAccumM f s' xs
-  return (s'', y : ys)
-
--- The function 'concatMapM' is a generalization of concatMap to monads
--- like foldM is for foldl.
-concatMapM :: Monad m => (a -> m [b]) -> [a] -> m [b]
-concatMapM f xs = mapM f xs >>= return . concat
-
 -- The function 'findDouble' checks whether a list of entities is linear,
 -- i.e., if every entity in the list occurs only once. If it is non-linear,
 -- the first offending object is returned.
+
 findDouble :: Eq a => [a] -> Maybe a
 findDouble []   = Nothing
 findDouble (x : xs)
