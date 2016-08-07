07be44ec1d90970028fdd22095958d7b1a457fad
Finn Teegen <fte@informatik.uni-kiel.de>
2016-08-08 00:53:00 +0200

    Finish checks

--- src/Checks.hs
+++ src/Checks.hs
@@ -30,6 +30,8 @@ import Curry.Base.Monad
 import Curry.Syntax (Module (..), Interface (..), ImportSpec)
 
 import Base.Messages
+import Base.Types
+
 import CompilerEnv
 import CompilerOpts
 
@@ -39,10 +41,11 @@ interfaceCheck :: Monad m => Check m Interface
 interfaceCheck _ (env, intf)
   | null msgs = ok (env, intf)
   | otherwise = failMessages msgs
-  where msgs = IC.interfaceCheck (opPrecEnv env) (tyConsEnv env)
-                                 (valueEnv env) intf
+  where msgs = IC.interfaceCheck (opPrecEnv env) (tyConsEnv env) (classEnv env)
+                                 (instEnv env) (valueEnv env) intf
 
-importCheck :: Monad m => Interface -> Maybe ImportSpec -> CYT m (Maybe ImportSpec)
+importCheck :: Monad m => Interface -> Maybe ImportSpec
+            -> CYT m (Maybe ImportSpec)
 importCheck intf is
   | null msgs = ok is'
   | otherwise = failMessages msgs
@@ -52,7 +55,7 @@ importCheck intf is
 --
 -- * Declarations: remain unchanged
 -- * Environment:  The enabled language extensions are updated
-extensionCheck :: Monad m => Check m Module
+extensionCheck :: Monad m => Check m (Module a)
 extensionCheck opts (env, mdl)
   | null msgs = ok (env { extensions = exts }, mdl)
   | otherwise = failMessages msgs
@@ -63,7 +66,7 @@ extensionCheck opts (env, mdl)
 -- * Declarations: Nullary type constructors and type variables are
 --                 disambiguated
 -- * Environment:  remains unchanged
-typeSyntaxCheck :: Monad m => Check m Module
+typeSyntaxCheck :: Monad m => Check m (Module a)
 typeSyntaxCheck _ (env, mdl)
   | null msgs = ok (env { extensions = exts }, mdl')
   | otherwise = failMessages msgs
@@ -72,31 +75,33 @@ typeSyntaxCheck _ (env, mdl)
 
 -- |Check the kinds of type definitions and signatures.
 --
--- * Declarations: remains unchanged
--- * Environment:  The type constructor environment is updated
-kindCheck :: Monad m => Check m Module
+-- * Declarations: remain unchanged
+-- * Environment:  The type constructor and class environment are updated
+kindCheck :: Monad m => Check m (Module a)
 kindCheck _ (env, mdl)
-  | null msgs = ok (env { tyConsEnv = tcEnv }, mdl)
+  | null msgs = ok (env { tyConsEnv = tcEnv', classEnv = clsEnv' }, mdl)
   | otherwise = failMessages msgs
-  where (tcEnv, msgs) = KC.kindCheck (tyConsEnv env) mdl
+  where ((tcEnv', clsEnv'), msgs) = KC.kindCheck (tyConsEnv env) (classEnv env)
+                                                 mdl
 
 -- |Check for a correct syntax.
 --
 -- * Declarations: Nullary data constructors and variables are
 --                 disambiguated, variables are renamed
 -- * Environment:  remains unchanged
-syntaxCheck :: Monad m => Check m Module
-syntaxCheck opts (env, mdl)
+syntaxCheck :: Monad m => Check m (Module ())
+syntaxCheck _ (env, mdl)
   | null msgs = ok (env { extensions = exts }, mdl')
   | otherwise = failMessages msgs
-  where ((mdl', exts), msgs) = SC.syntaxCheck opts (valueEnv env) mdl
+  where ((mdl', exts), msgs) = SC.syntaxCheck (extensions env) (tyConsEnv env)
+                                              (valueEnv env) mdl
 
 -- |Check the precedences of infix operators.
 --
 -- * Declarations: Expressions are reordered according to the specified
 --                 precedences
 -- * Environment:  The operator precedence environment is updated
-precCheck :: Monad m => Check m Module
+precCheck :: Monad m => Check m (Module a)
 precCheck _ (env, Module ps m es is ds)
   | null msgs = ok (env { opPrecEnv = pEnv' }, Module ps m es is ds')
   | otherwise = failMessages msgs
@@ -106,25 +111,28 @@ precCheck _ (env, Module ps m es is ds)
 --
 -- * Declarations: remain unchanged
 -- * Environment:  The instance environment is updated
-instanceCheck :: Monad m => Check m Module
+instanceCheck :: Monad m => Check m (Module a)
 instanceCheck _ (env, Module ps m es is ds)
   | null msgs = ok (env { instEnv = inEnv' }, Module ps m es is ds)
   | otherwise = failMessages msgs
   where (inEnv', msgs) = INC.instanceCheck (moduleIdent env) (tyConsEnv env)
-                                           (instEnv env) ds
+                                           (classEnv env) (instEnv env) ds
 
 -- |Apply the correct typing of the module.
--- The declarations remain unchanged; the type constructor and value
--- environments are updated.
-typeCheck :: Monad m => Check m Module
-typeCheck _ (env, mdl@(Module _ _ _ _ ds))
-  | null msgs = ok (env { tyConsEnv = tcEnv', valueEnv = tyEnv' }, mdl)
+--
+-- * Declarations: Type annotations are added to all expressions.
+-- * Environment:  The value environment is updated.
+typeCheck :: Monad m => Options -> CompEnv (Module a)
+          -> CYT m (CompEnv (Module PredType))
+typeCheck _ (env, Module ps m es is ds)
+  | null msgs = ok (env { valueEnv = vEnv' }, Module ps m es is ds')
   | otherwise = failMessages msgs
-  where (tcEnv', tyEnv', msgs) = TC.typeCheck (moduleIdent env)
-                                 (tyConsEnv env) (valueEnv env) ds
+  where (ds', vEnv', msgs) = TC.typeCheck (moduleIdent env) (tyConsEnv env)
+                                          (valueEnv env) (classEnv env)
+                                          (instEnv env) ds
 
 -- |Check the export specification
-exportCheck :: Monad m => Check m Module
+exportCheck :: Monad m => Check m (Module a)
 exportCheck _ (env, mdl@(Module _ _ es _ _))
   | null msgs = ok (env, mdl)
   | otherwise = failMessages msgs
@@ -132,13 +140,13 @@ exportCheck _ (env, mdl@(Module _ _ es _ _))
                               (tyConsEnv env) (valueEnv env) es
 
 -- |Check the export specification
-expandExports :: Monad m => Options -> CompEnv Module -> m (CompEnv Module)
+expandExports :: Monad m => Options -> CompEnv (Module a) -> m (CompEnv (Module a))
 expandExports _ (env, Module ps m es is ds)
   = return (env, Module ps m (Just es') is ds)
   where es' = EC.expandExports (moduleIdent env) (aliasEnv env)
                                (tyConsEnv env) (valueEnv env) es
 
 -- |Check for warnings.
-warnCheck :: Options -> CompilerEnv -> Module -> [Message]
+warnCheck :: Options -> CompilerEnv -> Module a -> [Message]
 warnCheck opts env mdl = WC.warnCheck (optWarnOpts opts) (aliasEnv env)
   (valueEnv env) (tyConsEnv env) mdl
