85bd7fc3a8704f409c092996ceffa12b31dbf101
Finn Teegen <fte@informatik.uni-kiel.de>
2016-10-21 15:40:58 +0200

    Fix incorrect type annotations in desugaring

--- src/Transformations/Desugar.hs
+++ src/Transformations/Desugar.hs
@@ -895,8 +895,8 @@ dsQual p (StmtBind r t l) e
     --TODO: Add SrcRefs (old version below)
     --v   <- addRefId r <$> freshVar "_#var" t
     --l'  <- addRefId r <$> freshVar "_#var" e
-    dsExpr p (apply (prelFoldr (typeOf t) (typeOf l) r)
-      [foldFunct v l' e, List (predType $ elemType $ typeOf l) [r] [], l])
+    dsExpr p (apply (prelFoldr (typeOf t) (typeOf e) r)
+      [foldFunct v l' e, List (predType $ typeOf e) [r] [], l])
   where
   qualExpr v (ListCompr _ e1 []) l1
     = apply (prelMap (typeOf v) (typeOf e1) r) [Lambda r [v] e1, l1]
